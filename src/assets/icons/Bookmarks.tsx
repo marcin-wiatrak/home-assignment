0ed6f7cbe2f0a84a26c8c0da27dcbabac59e1a49
Marcin Wiatrak <marcin.wiatrak.95@gmail.com>
2024-08-11 13:51:27 +0200

    feat: SD-02 Import all SVGs

---
+++ src/assets/icons/Bookmarks.tsx
@@ -0,0 +1,21 @@
+export const BookmarksIcon = () => (
+  <svg
+    width={24}
+    height={24}
+    viewBox="0 0 24 24"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <g clipPath="url(#clip0_4601_433)">
+      <path
+        d="M11 5H5v14h6V5zm2 0v14h6V5h-6zM4 3h16a1 1 0 011 1v16a1 1 0 01-1 1H4a1 1 0 01-1-1V4a1 1 0 011-1z"
+        fill="#007DFC"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_4601_433">
+        <path fill="#fff" d="M0 0H24V24H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+)
---
+++ src/assets/icons/Home.tsx
@@ -0,0 +1,15 @@
+export const HomeIcon = () => (
+  <svg width={24} height={24} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <g clipPath="url(#clip0_4601_430)">
+      <path
+        d="M19 21H5a1 1 0 01-1-1v-9H1l10.327-9.388a1 1 0 011.346 0L23 11h-3v9a1 1 0 01-1 1zM6 19h12V9.157l-6-5.454-6 5.454V19zm2-4h8v2H8v-2z"
+        fill="#B7BEC5"
+      />
+    </g>
+    <defs>
+      <clipPath id="clip0_4601_430">
+        <path fill="#fff" d="M0 0H24V24H0z" />
+      </clipPath>
+    </defs>
+  </svg>
+)
