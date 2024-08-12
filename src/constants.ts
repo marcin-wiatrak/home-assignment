3dd4c574e685fd470fba1b1d3898380beec6a65f
Marcin Wiatrak <marcin.wiatrak.95@gmail.com>
2024-08-12 10:37:21 +0200

    feat: reorganize tasks list mock

--- src/constants.ts
+++ src/constants.ts
@@ -1,61 +1,87 @@
+import { TTodo } from './types.ts'
+import { v4 as uuidv4 } from 'uuid'
+
 export const TODOS: TTodo[] = [
-    {
-        id: '1',
-        createdAt: '2021-07-01T00:00:00.000Z',
-        description: 'Go to gym',
-        isCompleted: false,
-    },
-    {
-        id: '2',
-        createdAt: '2021-07-01T00:00:00.000Z',
-        description: 'Go shopping',
-        isCompleted: false,
-    },
-    {
-        id: '3',
-        createdAt: '2021-07-01T00:00:00.000Z',
+  {
+    id: `task-${uuidv4()}`,
+    description: 'Go to gym',
+    isCompleted: false,
+    subtasks: []
+  },
+  {
+    id: `task-${uuidv4()}`,
+    description: 'Go shopping',
+    isCompleted: false,
+    subtasks: [
+      {
+        id: `task-${uuidv4()}`,
         description: 'Buy bread',
         isCompleted: false,
-        parentId: '2'
-    },
-    {
-        id: '4',
-        createdAt: '2021-07-01T00:00:00.000Z',
-        description: 'Buy milk',
+        subtasks: []
+      },
+      {
+        id: `task-${uuidv4()}`,
+        description: 'Buy bread',
         isCompleted: false,
-        parentId: '2'
-    },
-    {
-        id: '5',
-        createdAt: '2021-07-01T00:00:00.000Z',
-        description: 'Watch movie',
+        subtasks: [
+          {
+            id: `task-${uuidv4()}`,
+            description: 'Buy bread',
+            isCompleted: false,
+            subtasks: []
+          },
+        ]
+      },
+      {
+        id: `task-${uuidv4()}`,
+        description: 'Buy bread',
         isCompleted: false,
-    },
-    {
-        id: '6',
-        createdAt: '2021-07-01T00:00:00.000Z',
+        subtasks: []
+      },
+    ]
+  },
+  {
+    id: `task-${uuidv4()}`,
+    description: 'Buy bread',
+    isCompleted: false,
+    subtasks: []
+  },
+  {
+    id: `task-${uuidv4()}`,
+    description: 'Buy milk',
+    isCompleted: false,
+    subtasks: []
+  },
+  {
+    id: `task-${uuidv4()}`,
+    description: 'Watch movie',
+    isCompleted: false,
+    subtasks: []
+  },
+  {
+    id: `task-${uuidv4()}`,
+    description: 'Prepare for next day',
+    isCompleted: false,
+    subtasks: [
+      {
+        id: `task-${uuidv4()}`,
         description: 'Do homework',
         isCompleted: false,
-    },
-    {
-        id: '7',
-        createdAt: '2021-07-01T00:00:00.000Z',
-        description: 'Prepare for next day',
-        isCompleted: false,
-        parentId: '6'
-    },
-    {
-        id: '8',
-        createdAt: '2021-07-01T00:00:00.000Z',
+        subtasks: []
+      },
+      {
+        id: `task-${uuidv4()}`,
         description: 'Buy some food for tomorrow',
         isCompleted: false,
-        parentId: '6'
-    },
-    {
-        id: '9',
-        createdAt: '2021-07-01T00:00:00.000Z',
-        description: 'Pack up food',
-        isCompleted: false,
-        parentId: '8'
-    },
+        subtasks: [
+          {
+            id: `task-${uuidv4()}`,
+            description: 'Pack up food',
+            isCompleted: false,
+            subtasks: []
+          },
+        ]
+      },
+    ]
+  },
 ]
